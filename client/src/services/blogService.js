8c4a8009997a22cd3004685f90ec20b9810c9e5a
Ali Ilgaz Karatas <80777882+aliilgazkaratas@users.noreply.github.com>
2026-02-13 18:36:12 -0500

    Add default export to blogService

--- client/src/services/blogService.js
+++ client/src/services/blogService.js
@@ -3,37 +3,41 @@ import api from './api';
 // Get all blogs
 export const getAllBlogs = async (params = {}) => {
   const queryString = new URLSearchParams(params).toString();
-  const response = await api.get(`/api/blogs?${queryString}`);
+  const response = await api.get(`/blogs?${queryString}`);
   return response.data;
 };
 
-// Get single blog by ID
+// Get single blog
 export const getBlogById = async (id) => {
-  const response = await api.get(`/api/blogs/${id}`);
+  const response = await api.get(`/blogs/${id}`);
   return response.data;
 };
 
-// Create new blog (admin only)
+// Create blog (admin)
 export const createBlog = async (blogData) => {
-  const response = await api.post('/api/blogs', blogData);
+  const response = await api.post('/blogs', blogData);
   return response.data;
 };
 
-// Update blog (admin only)
+// Update blog (admin)
 export const updateBlog = async (id, blogData) => {
-  const response = await api.put(`/api/blogs/${id}`, blogData);
+  const response = await api.put(`/blogs/${id}`, blogData);
   return response.data;
 };
 
-// Delete blog (admin only)
+// Delete blog (admin)
 export const deleteBlog = async (id) => {
-  const response = await api.delete(`/api/blogs/${id}`);
+  const response = await api.delete(`/blogs/${id}`);
   return response.data;
 };
 
-// Get blogs by author
-export const getBlogsByAuthor = async (authorId, params = {}) => {
-  const queryString = new URLSearchParams(params).toString();
-  const response = await api.get(`/api/blogs/author/${authorId}?${queryString}`);
-  return response.data;
+// ADD THIS DEFAULT EXPORT AT THE END:
+const blogService = {
+  getAllBlogs,
+  getBlogById,
+  createBlog,
+  updateBlog,
+  deleteBlog
 };
+
+export default blogService;
