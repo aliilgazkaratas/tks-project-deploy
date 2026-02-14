a74f109c0bd479a469a3465d9683609c7107bfbe
Ali Ilgaz Karatas <80777882+aliilgazkaratas@users.noreply.github.com>
2026-02-13 23:23:59 -0500

    Add EventProvider wrapper

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
 import { AuthProvider } from './context/AuthContext';
+import { EventProvider } from './context/EventContext';  // ADD THIS
 import './styles/modern-theme.css';
 import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      <RouterProvider router={router} />
+      <EventProvider>  {/* ADD THIS */}
+        <RouterProvider router={router} />
+      </EventProvider>  {/* ADD THIS */}
     </AuthProvider>
   </React.StrictMode>
 );
